3005387dcb716fb48d32a88b91cf1312987d5df2
AllanEl <allanrnelias@gmail.com>
2023-02-23 17:33:37 -0500

    goes to homepage when click on logo

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -39,7 +39,7 @@ const Profile = () => {
     <div>
       <div className="flex-row justify-center mb-3">
 
-        <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
+        <h2 className="col-12 col-md-10 bg-info text-light p-3 mb-5">
           Welcome, {Auth.getProfile().data.username}
 
 
