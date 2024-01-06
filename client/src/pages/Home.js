05bebdbbf3d73215d1b92bee1c657c0a21d61fe0
Allan Elias <111256927+EliasAllan@users.noreply.github.com>
2024-01-06 01:10:09 -0500

    simplified html in homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,34 +1,27 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
+import React from "react";
+import { useQuery } from "@apollo/client";
 
-import DeckList from '../components/DeckList';
+import DeckList from "../components/DeckList";
 // import DeckForm from '../components/DeckForm';
 
-import { QUERY_DECKS } from '../utils/queries';
+import { QUERY_DECKS } from "../utils/queries";
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_DECKS);
   const decks = data?.decks || [];
-  console.log(data)
+  console.log(data);
   return (
     <main>
-      <div  className="flex-row justify-center">
-        <div
-          id="deck-container"
-          className="col-12 col-md-10 mb-3 p-3"
-        >
-         {loading ? (
+        <div id="deck-container">
+          {loading ? (
             <div>Loading...</div>
           ) : (
             <DeckList
-            title="Subjects other users studied recently ..."
-            decks={decks}
+              title="Subjects other users studied recently ..."
+              decks={decks}
             />
           )}
         </div>
-        <div className="col-12 col-md-8 mb-3">
-        </div>
-      </div>
     </main>
   );
 };
