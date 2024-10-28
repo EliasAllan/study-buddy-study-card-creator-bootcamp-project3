eb05ab7fa83369a5c8b0f8af4e5af77766e0e291
allan elias <allanrnelias@gmail.com>
2024-10-28 01:18:23 -0400

    hotfix3

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,7 @@ mongoose.set('strictQuery', false);
 
 // Creating a connection to mongodb using mongoose with database name.
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/study-buddy',
+  process.env.MONGODB_URI || 'mongodb+srv://allanrnelias:<db_password>@study-buddy.fix9my7.mongodb.net/',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
