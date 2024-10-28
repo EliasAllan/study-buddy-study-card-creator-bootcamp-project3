bd7fd610ef6bcf1daad24b30c302748c0bb2b97a
allan elias <allanrnelias@gmail.com>
2024-10-28 00:05:53 -0400

    fixed connection string

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,7 @@ mongoose.set('strictQuery', false);
 
 // Creating a connection to mongodb using mongoose with database name.
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/study-buddy',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
